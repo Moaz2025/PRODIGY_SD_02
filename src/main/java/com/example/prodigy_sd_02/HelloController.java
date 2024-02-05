5bb19f2a92f9bdcf9716f94fa6d0013e6a5c5c96
Moaz2025 <moaz20ahmed@gmail.com>
2024-02-05 11:58:56 +0200

    Implement GUI and some modifications in generation logic

--- src/main/java/com/example/prodigy_sd_02/HelloController.java
+++ src/main/java/com/example/prodigy_sd_02/HelloController.java
@@ -1,14 +1,55 @@
 package com.example.prodigy_sd_02;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
+import java.util.Objects;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+    Generator generator;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Text attempts;
+
+    @FXML
+    private Text generated;
+
+    @FXML
+    private TextField guessInput;
+
+    @FXML
+    private Text output;
+
+    @FXML
+    private TextField rangeInput;
+
+    @FXML
+    void checkInput(MouseEvent event) {
+        if (Objects.isNull(generator))
+            output.setText("No number generated");
+        else {
+            String input = guessInput.getText();
+            NumericCheck numericCheck = new NumericCheck();
+            if (numericCheck.isNumeric(input)) {
+                String check = generator.checkValue(Integer.parseInt(guessInput.getText()));
+                output.setText("Status: " + check);
+                attempts.setText("Number of attempts: " + generator.getAttempts());
+            }
+            else output.setText("Enter a valid number");
+        }
+    }
+
+    @FXML
+    void generate(MouseEvent event) {
+        String input = rangeInput.getText();
+        NumericCheck numericCheck = new NumericCheck();
+        if (numericCheck.isNumeric(input)) {
+            this.generator = new Generator();
+            generator.start(Integer.parseInt(rangeInput.getText()));
+            generated.setText("Number generated successfully");
+        }
+        else generated.setText("Enter a valid number");
     }
 }
---
+++ src/main/java/com/example/prodigy_sd_02/NumericCheck.java
@@ -0,0 +1,16 @@
+package com.example.prodigy_sd_02;
+
+public class NumericCheck {
+
+    public boolean isNumeric(String strNum) {
+        if (strNum == null) {
+            return false;
+        }
+        try {
+            double d = Double.parseDouble(strNum);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
+    }
+}
